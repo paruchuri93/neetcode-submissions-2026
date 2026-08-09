9dcb4436770262547c73f1b9e0c6b82a12739a72
paruchuri93 <paruchuri93@gmail.com>
2026-08-08 21:25:34 -0400

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if (s.length !== t.length) {
+            return false;
+        }
+
+        const charString = new Array(26).fill(0);
+
+        for (let i = 0; i < s.length; i++) {
+            charString[s.charCodeAt(i) - "a".charCodeAt(0)]++;
+            charString[t.charCodeAt(i) - "a".charCodeAt(0)]--;
+        }
+
+        return charString.every((val) => val === 0);
+    }
+}
