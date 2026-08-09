a069d017b85bb280d2d70259661af834845fa794
paruchuri93 <paruchuri93@gmail.com>
2026-08-08 21:47:19 -0400

    Add: buy-and-sell-crypto - submission-24

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-24.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        maxP = 0
+        left =0
+        right =1
+
+        while(right < len(prices)):
+            if(prices[right] > prices[left]):
+                maxP = max(maxP, prices[right] - prices[left])
+            else:
+                left = right
+            right +=1
+
+        return maxP
+        
