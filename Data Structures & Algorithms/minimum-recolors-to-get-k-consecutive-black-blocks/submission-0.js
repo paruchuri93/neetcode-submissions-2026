eba3264a663d3557c160a78d49b00cc2603276f0
paruchuri93 <paruchuri93@gmail.com>
2026-08-26 21:55:00 -0400

    Add: minimum-recolors-to-get-k-consecutive-black-blocks - submission-0

---
+++ Data Structures & Algorithms/minimum-recolors-to-get-k-consecutive-black-blocks/submission-0.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {string} blocks
+     * @param {number} k
+     * @return {number}
+     */
+    minimumRecolors(blocks, k) {
+        let currOperations = 0;
+
+        for (let i = 0; i < k; i++) {
+            if (blocks[i] === "W") {
+                currOperations += 1;
+            }
+        }
+
+        let minOperations = currOperations;
+        for(let i=k; i<blocks.length; i++){
+            if (blocks[i] === "W") {
+                currOperations += 1;
+            }
+
+            if(blocks[i-k] === "W"){
+                currOperations -= 1;
+            }
+
+            minOperations = Math.min(currOperations, minOperations)
+        }
+
+        return minOperations;
+    }
+}
