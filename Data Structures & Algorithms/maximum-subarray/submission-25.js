3c572a66f5a88be6825fd9f24bba23da003b246b
paruchuri93 <paruchuri93@gmail.com>
2026-09-10 20:24:06 -0400

    Add: maximum-subarray - submission-25

---
+++ Data Structures & Algorithms/maximum-subarray/submission-25.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    maxSubArray(nums) {
+        let curr = 0;
+        let maxSum = -Infinity;
+
+        for (let i =0; i<nums.length; i++) {
+            if(curr < 0) {
+                curr = 0;
+            }
+            curr+=nums[i];
+            if(maxSum < curr){
+                maxSum = curr;
+            }
+        }
+
+        return maxSum;
+    }
+}
