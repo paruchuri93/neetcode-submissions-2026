5800a77722f8fcd756006f555da7af331587c329
paruchuri93 <paruchuri93@gmail.com>
2026-09-15 18:01:40 -0400

    Add: remove-duplicates-from-sorted-array - submission-8

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-8.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    removeDuplicates(nums) {
+        let l = 0;
+        let r = 1;
+
+        while (r < nums.length) {
+            if (nums[l] !== nums[r]) {
+                l++;
+                nums[l] = nums[r]
+            }
+            r++;
+        }
+        return l + 1;
+    }
+}
