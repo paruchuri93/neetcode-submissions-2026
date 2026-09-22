02d21308c71a07870dda2c84e6682d3c693c37b8
paruchuri93 <paruchuri93@gmail.com>
2026-09-21 20:04:29 -0400

    Add: max-consecutive-ones - submission-6

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-6.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findMaxConsecutiveOnes(nums) {
+        let curr = 0;
+        let maxLen = 0;
+        for (let r = 0; r < nums.length; r++) {
+            if (nums[r] === 0) {
+                curr = 0;
+            } else {
+                curr++;
+            }
+            maxLen = Math.max(maxLen, curr);
+        }
+
+        return maxLen;
+    }
+}
