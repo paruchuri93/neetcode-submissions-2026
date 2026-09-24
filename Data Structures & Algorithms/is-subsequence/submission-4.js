a8a394f9b9f73a9b1e09e56cac23d3cc347d08bc
paruchuri93 <paruchuri93@gmail.com>
2026-09-23 20:49:23 -0400

    Add: is-subsequence - submission-4

---
+++ Data Structures & Algorithms/is-subsequence/submission-4.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isSubsequence(s, t) {
+        if (t.length < s.length) return false;
+
+        let left = 0;
+        for (let right = 0; left < s.length && right < t.length; right++) {
+            if (t[right] === s[left]) {
+                left++;
+            }
+        }
+
+        return left === s.length;
+    }
+}
