f19e3da0af0ade57830a2b91560c41e8457c57e4
paruchuri93 <paruchuri93@gmail.com>
2026-09-26 10:22:12 -0400

    Add: anagram-groups - submission-11

---
+++ Data Structures & Algorithms/anagram-groups/submission-11.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        let groups = {};
+
+        for (let i = 0; i < strs.length; i++) {
+            let curr = strs[i];
+            let hash = new Array(26).fill(0);
+            for (let ch of curr) {
+                let index = ch.charCodeAt(0) - "a".charCodeAt(0);
+                hash[index] = (hash[index] || 0) + 1;
+            }
+            const key = hash.join(",");
+            if (key in groups) {
+                groups[key].push(curr);
+            } else {
+                groups[key] = [curr];
+            }
+        }
+
+        return Object.values(groups);
+    }
+}
